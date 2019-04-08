24c96b4f7e3820cb38b7869ff47ab8fc3accc952
Noah G <NoahBGlasser@gmail.com>
2019-04-08 13:45:36 -0700

    geolocation

---
+++ public/javascript/eventListeners.js
@@ -0,0 +1,37 @@
+var currentUrl = window.location.origin
+var pos = {}
+
+$(function() {
+    // $("#login").on("click", function(event){
+    //     event.preventDefault();
+
+    //     var userInfo = {
+    //         userName: $("#user-name").val(),
+    //         password: $("#password").val()
+    //     }
+    // })
+
+    // $.ajax("/api/USERS", {
+    //     type: "POST",
+    //     data: userInfo
+    // }).then(
+    //     function(data){
+    //         if(data){
+    //            window.location = currentUrl + "/home"
+    //         }
+    //     }
+    // )
+
+    $("#sumbit").on("click", function(event){
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(pos)
+        })
+ 
+    })
+
+
+})
