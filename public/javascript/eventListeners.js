cd4187f13b321c76c995e74bb956b7f03369b67d
Noah G <NoahBGlasser@gmail.com>
2019-04-08 14:18:02 -0700

    ajax

--- public/javascript/eventListeners.js
+++ public/javascript/eventListeners.js
@@ -28,9 +28,22 @@ $(function() {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
-            console.log(pos)
+            
         })
- 
+        $.ajax("/api/spots", {
+            type: "POST",
+            data: pos
+        }).then(
+            window.location = currentUrl + "/home"
+        )
+    })
+
+    $("#search").on("click", function(event){
+        $.get("/api/spots", function(data){
+            console.log(data)
+        }).then(
+            location.reload()
+        )   
     })
 
 
