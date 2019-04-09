2e1342861bbfd0bdf4b60fc598e3cfd3362fd017
Noah G <NoahBGlasser@gmail.com>
2019-04-09 13:20:38 -0700

    map logic

--- public/javascript/eventListeners.js
+++ public/javascript/eventListeners.js
@@ -1,6 +1,7 @@
 var currentUrl = window.location.origin
 var pos = {}
 
+
 $(function() {
     // $("#login").on("click", function(event){
     //     event.preventDefault();
@@ -46,5 +47,55 @@ $(function() {
         )   
     })
 
+
+    var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+          navigator.geolocation.getCurrentPosition(function(position) {
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+            
+          });
+         
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                'Error: The Geolocation service failed.' :
+                'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+      var script = document.createElement('script');
+        script.src = currentUrl + '/api/spots';
+        document.getElementsByTagName('head')[0].appendChild(script);
+      ,
+
+      // Loop through the results array and place a marker for each
+      // set of coordinates.
+      window.eqfeed_callback = function(results) {
+        for (var i = 0; i < results.features.length; i++) {
+          var coords = results.features[i].geometry.coordinates;
+          var latLng = new google.maps.LatLng(coords[1],coords[0]);
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          });
+        }
+      }
+    
+   
 
 })
