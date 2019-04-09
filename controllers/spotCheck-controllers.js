c81e79ea6eabba53412b33f12f027a6b8b5a48b7
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-09 15:39:46 -0700

    add some event listeners on client-side and handlebars files to have a login page and a main page, and some routes on spotCheck-controllers.js

--- controllers/spotCheck-controllers.js
+++ controllers/spotCheck-controllers.js
@@ -16,10 +16,22 @@ var express = require("express");
 // create an object to perform the routing functions
 var router = express.Router();
 
-// define the route to display the main page
+// define the route to display the "login" page
 router.get("/", function(req, res) {
     res.render("index");
 });
+
+// define the route to display the page with the map and the spots
+router.get("/home", function(req, res) {
+    res.render("home");
+});
+
+// to store the phone number into the users model
+router.post("/api/users", function(req, res) {
+
+    res.end();
+});
+
 
 
 // Export the router to make it available for other files (i. e., the server.js)
--- public/javascript/eventListeners.js
+++ public/javascript/eventListeners.js
@@ -1,16 +1,35 @@
 // load the page before running anything else
 $(document).ready(function() {
 
-    // search form not displayed by default
-    var displayForm = false;
-    
-    // if the user clicks on "At Destination", the search form appears 
-    $("#near-you").on("click", function() {
-        displayForm = false;
+    // event listener on the "submit" button on the login page
+    $("#login-form").on("submit", function(event) {
+        // prevent the page to refresh
+        event.preventDefault();
+
+        // grab the user phone number and store it in a variable
+        var phoneNumber = $("#phone-number").val().trim();
+        
+        // post request to enter the data into the database
+        $.post("/api/users", phoneNumber, function() {
+            // very short-live confirmation message in the browser's console
+            console.log("created new user");
+            // display the home page with map and spots
+            location.replace("/home");
+        });
+
     });
 
-    // if the user clicks on "Near You", the search form disappears 
-    $("#destination").on("click", function() {
-        displayForm = true;
+    // hide the "search-destination" bar for "near you" option
+    $("#near-you").on("click", function() {
+        $("#search-destination").attr("style", "display:none");
     });
+
+    // display the "search-destination" bar for "At Destination" option
+    $("#destination").on("click", function() {
+        $("#search-destination").attr("style", "display:block");
+    })
+
+
+
+    
 });
