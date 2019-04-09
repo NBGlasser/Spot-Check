22d8be0fa0342d35054376744e75ecbfbd00baa5
Noah G <NoahBGlasser@gmail.com>
2019-04-09 13:29:29 -0700

    map logic tweaks

--- public/javascript/eventListeners.js
+++ public/javascript/eventListeners.js
@@ -51,12 +51,18 @@ $(function() {
     var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
+          center: {lat: -0, lng: 0},
           zoom: 6
         });
         infoWindow = new google.maps.InfoWindow;
 
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
@@ -64,9 +70,11 @@ $(function() {
             map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
-            
           });
-         
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
       }
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -81,12 +89,13 @@ $(function() {
       var script = document.createElement('script');
         script.src = currentUrl + '/api/spots';
         document.getElementsByTagName('head')[0].appendChild(script);
-      ,
+      
 
       // Loop through the results array and place a marker for each
       // set of coordinates.
       window.eqfeed_callback = function(results) {
         for (var i = 0; i < results.features.length; i++) {
+            //===================== CHECK THE JSON WHEN WE MERGE=================//
           var coords = results.features[i].geometry.coordinates;
           var latLng = new google.maps.LatLng(coords[1],coords[0]);
           var marker = new google.maps.Marker({
